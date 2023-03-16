a60a7aefaa44bc78af93f5e20aea27787b989398
alpha0212 <qkrcjfgml321@naver.com>
2023-03-16 10:51:45 +0900

    The absolute path to get the page has been modified to a relative path.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { MainPage } from '@/pages';
+import { MainPage } from './pages';
 
 export const App: React.FC = () => {
   return (
-    <Routes>
-      <Route index element={<MainPage />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route index element={<MainPage />} />
+      </Routes>
+    </>
   );
 };
