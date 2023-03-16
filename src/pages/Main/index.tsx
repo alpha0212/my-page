03c4c87ee6373829692a96b05e6f3873ecff90d1
alpha0212 <qkrcjfgml321@naver.com>
2023-03-16 11:03:36 +0900

    feat(Main/index.tsx): Add section components

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
 import * as S from './styled';
+import { FirstSection, SecondSection } from '../../components';
 
-export const MainPage: React.FC = () => <></>;
+export const MainPage: React.FC = () => {
+  return (
+    <>
+      <S.MainPageContainer>
+        <FirstSection />
+        <SecondSection />
+      </S.MainPageContainer>
+    </>
+  );
+};
--- src/pages/Main/styled.ts
+++ src/pages/Main/styled.ts
@@ -1,1 +1,10 @@
 import styled from '@emotion/styled';
+
+export const MainPageContainer = styled.main`
+  height: 100vh;
+  overflow-y: auto;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  scroll-behavior: smooth;
+`;
