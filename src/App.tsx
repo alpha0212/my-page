72feafa124184e21ca181506b86c6a584693da81
alpha0212 <qkrcjfgml321@naver.com>
2023-03-17 21:10:05 +0900

    feat(App.tsx): Data send to navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { MainPage } from './pages';
+import { Navbar } from './components';
 
 export const App: React.FC = () => {
   return (
     <>
+      <Navbar
+        Logo="DEV-JUN"
+        Project="PROJECT"
+        ProjectGo="project"
+        Contact="CONTACT"
+        ContactGo="contact"
+        NavbarShowAndHide={true}
+      />
       <Routes>
         <Route index element={<MainPage />} />
       </Routes>
